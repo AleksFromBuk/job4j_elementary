de235d1ab0bc8e47a108af5eb614c7c24e72e33b
AleksFromBuk <layko1982@mail.ru>
2021-02-22 17:06:50 +0300

    9.1. String [#444451]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -1,6 +1,6 @@
 package ru.job4j;
 
-public class Info{
+public class Info {
     public static void main(String[] args) {
         System.out.println("14.02.2021");
     }
--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -1,15 +1,15 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(int p, int k) {
+    public static double square(double p, double k) {
         double h = p / (2 * (k + 1));
-        double L = h * k;
-        return L * h;
+        double l = h * k;
+        return l * h;
     }
+
     public static void main(String[] args) {
         double result1 = SqArea.square(4, 1);
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
-
         result1 = SqArea.square(6, 2);
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
     }
