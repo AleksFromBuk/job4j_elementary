75337e076fc1ad2a4b01a3bfa7ac2221d7e499e2
AleksFromBuk <layko1982@mail.ru>
2021-02-23 16:07:21 +0300

    0. Что такое тестирование [#444473]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -18,6 +18,7 @@ public class Converter {
         int out = Converter.rubleToEuro(in);
         boolean passed = expected == out;
         System.out.println("140 rubles are 2. Test result : " + passed);
+        //asdfadsf
 
         in = 17;
         expected = 0;
