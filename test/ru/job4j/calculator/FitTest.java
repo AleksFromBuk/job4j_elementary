c96b3063fe852749b2e3024d13e96d3344e191fa
AleksFromBuk <layko1982@mail.ru>
2021-02-25 15:24:09 +0300

    3. Тесты для определения расстояния между точками. [#444476]

--- test/ru/job4j/calculator/FitTest.java
+++ test/ru/job4j/calculator/FitTest.java
@@ -17,7 +17,7 @@ public class FitTest {
     public void whenWoman170Then69() {
         short in = 170;
         double out = Fit.womanWeight(in);
-        double expected = 68.989999999999999999999999999999999999999999999999999999999999999999999999999999;
+        double expected = 69;
         Assert.assertEquals(out, expected, 0.01);
     }
 }
