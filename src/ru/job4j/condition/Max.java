c5ff535dddd4f3b2eeadfdde0fa52fb973343622
AleksFromBuk <layko1982@mail.ru>
2021-02-25 23:33:09 +0300

    4.1.2. Глупый бот [#444384]

---
+++ src/ru/job4j/condition/Max.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int ma(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ma(3, 4));
+    }
+}
