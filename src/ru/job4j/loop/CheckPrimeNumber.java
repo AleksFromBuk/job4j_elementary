464d4a76242ae01689d68906814bf584ede90dfc
AleksFromBuk <layko1982@mail.ru>
2021-02-27 16:26:35 +0300

    5.4. Протеиновая диета [#444498]

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,23 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+
+    public static boolean check(int number) {
+        if (number == 1) {
+            return false;
+        }
+        boolean prime = true;
+        for (int i = 2; i <= number - 1; i++) {
+            if (number % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+
+    static public void main(String[] args) {
+        System.out.println(check(1));
+    }
+
+}
