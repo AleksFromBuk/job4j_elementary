bb86ffc6e2ec03738aeb1d6e61ccd7ff4662807d
AleksFromBuk <layko1982@mail.ru>
2021-03-01 15:39:56 +0300

    6.0.2. Заполнение массива. [#444422]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -9,6 +9,16 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
-        //for commit
+
+        String[] names = new String[4];
+        names[0] = "Bugaga";
+        names[1] = names[0] + "agauB";
+        names[2] = " - is";
+        names[3] = "palindrome";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
