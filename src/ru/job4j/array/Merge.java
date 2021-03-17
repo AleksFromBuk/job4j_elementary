9026acdd3630072a6465f3954bf5c17e21dd8d09
AleksFromBuk <layko1982@mail.ru>
2021-03-17 22:59:12 +0300

    3. Объединить два отсортированных массива (к экзамену по Базовму синтаксису)

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -3,7 +3,34 @@ package ru.job4j.array;
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int id = 0;
+        if (left.length == 0 ||  right.length == 0) {
+            rsl = right.length == 0 ?  left : right;
+            return rsl;
+        }
+        for (int i = 0, j = 0, k = 0; i < rsl.length;) {
+
+            if (j == left.length) {
+                while (i < rsl.length) {
+                    rsl[i++] = right[k++];
+                }
+                return rsl;
+            }
+            if (k == right.length) {
+                while (i < rsl.length) {
+                    rsl[i++] = left[j++];
+                }
+                return rsl;
+            }
+            if (left[j] < right[k]) {
+                rsl[i++] = left[j];
+                j++;
+            } else {
+                rsl[i++] = right[k];
+                k++;
+            }
+        }
+
+        /*int id = 0;
         if (left.length == 0 ||  right.length == 0) {
             rsl = right.length == 0 ?  left : right;
             return rsl;
@@ -27,7 +54,7 @@ public class Merge {
                     rsl[id++] = left[i++];
                 }
             }
-        }
+        }*/
         return rsl;
     }
 
